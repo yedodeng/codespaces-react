de244c8d7603e2078262ee32992aa89017dd6fd7
yedodeng <143571656+yedodeng@users.noreply.github.com>
2024-02-14 03:24:55 +0000

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <div>club app</div>;
-}
+import { useEffect } from "react";
+import { supabase } from "./supabaseClient";
 
-export default App;
+export default function App() {
+  async function loadData() {
+    console.log("loading data");
+    const { data, error } = await supabase.from("items").select("*");
+    console.log(data, error);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return <div>Club App</div>;
+}
